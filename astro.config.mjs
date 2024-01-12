bb42c8d1f6ce7b83427ba3d15a2790be2087ad40
Nathan Fellows <nfellowsdesign@gmail.com>
2024-01-12 11:51:30 -0800

    second push

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-site: 'https://nfellowsdesign.github.io',
+site: 'https://nathanfellows.design',
 });
